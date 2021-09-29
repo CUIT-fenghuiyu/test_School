85c79f79eac66c0b4f47554ac435b1c42d273443
CUIT-fenghuiyu <2959404472@qq.com>
2021-09-29 22:59:53 +0800

    Stack

---
+++ Stack/Stack/Stack.h
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+#include<assert.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// ̬
+//#define N 100
+//typedef int STDatatype;
+//struct Stack
+//{
+//	STDatatype a[N];
+//	int top;    
+//};
+
+typedef int STDatatype;
+typedef struct Stack
+{
+	STDatatype* a;
+	int top;		
+	int capacity;
+}Stack;
+
+void StackInit(Stack* stack);
+void StackDestroy(Stack* stack);
+void StackPush(Stack* stack, STDatatype x);
+void StackPop(Stack* stack);
+bool StackEmpty(Stack* stack);
+int StackSize(Stack* stack);
+STDatatype StackTop(Stack* stack);
